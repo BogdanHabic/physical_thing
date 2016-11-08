1a8d9a6ef161da122d5edfed5dee0ae26fdb4f8d
Lazar Bulić <lazarbulic@gmail.com>
2016-11-08 13:37:34 +0100

    Add files via upload

---
+++ master/screen.asm
@@ -0,0 +1,193 @@
+_USB0Interrupt:
+;screen.c,39 :: 		void USB0Interrupt() iv IVT_INT_OTG_FS{
+SUB	SP, SP, #4
+STR	LR, [SP, #0]
+;screen.c,40 :: 		USB_Interrupt_Proc();
+BL	_USB_Interrupt_Proc+0
+;screen.c,41 :: 		}
+L_end_USB0Interrupt:
+LDR	LR, [SP, #0]
+ADD	SP, SP, #4
+BX	LR
+; end of _USB0Interrupt
+_Timer2_interrupt:
+;screen.c,43 :: 		void Timer2_interrupt() iv IVT_INT_TIM2 {	// iv-> hendler za tajmerski prekid
+;screen.c,44 :: 		TIM2_SR.UIF = 0;				//Kada se prekid desi, postavlja se fleg TIM2_SR.UIF
+MOVS	R1, #0
+SXTB	R1, R1
+MOVW	R0, #lo_addr(TIM2_SR+0)
+MOVT	R0, #hi_addr(TIM2_SR+0)
+STR	R1, [R0, #0]
+;screen.c,45 :: 		if(curr_state == STATE_RUNNING) {
+MOVW	R0, #lo_addr(_curr_state+0)
+MOVT	R0, #hi_addr(_curr_state+0)
+LDRSH	R0, [R0, #0]
+CMP	R0, #1
+IT	NE
+BNE	L_Timer2_interrupt0
+;screen.c,46 :: 		seconds++;
+MOVW	R1, #lo_addr(_seconds+0)
+MOVT	R1, #hi_addr(_seconds+0)
+LDRSH	R0, [R1, #0]
+ADDS	R0, R0, #1
+STRH	R0, [R1, #0]
+;screen.c,47 :: 		}
+L_Timer2_interrupt0:
+;screen.c,48 :: 		}
+L_end_Timer2_interrupt:
+BX	LR
+; end of _Timer2_interrupt
+_Initialize:
+;screen.c,50 :: 		void Initialize() {
+SUB	SP, SP, #4
+STR	LR, [SP, #0]
+;screen.c,51 :: 		RCC_APB1ENR.TIM2EN = 1;       // koristimo TIM2
+MOVS	R1, #1
+SXTB	R1, R1
+MOVW	R0, #lo_addr(RCC_APB1ENR+0)
+MOVT	R0, #hi_addr(RCC_APB1ENR+0)
+STR	R1, [R0, #0]
+;screen.c,52 :: 		TIM2_CR1.CEN = 0;             // privremeno iskljuèujemo TIM2
+MOVS	R1, #0
+SXTB	R1, R1
+MOVW	R0, #lo_addr(TIM2_CR1+0)
+MOVT	R0, #hi_addr(TIM2_CR1+0)
+STR	R1, [R0, #0]
+;screen.c,53 :: 		TIM2_PSC = 1098;              // podesavamo preskaler(tj dvobajtnu vrednost sa kojom se deli frekvencija tajmera(tj TIM2_ARR) )
+MOVW	R1, #1098
+MOVW	R0, #lo_addr(TIM2_PSC+0)
+MOVT	R0, #hi_addr(TIM2_PSC+0)
+STR	R1, [R0, #0]
+;screen.c,54 :: 		TIM2_ARR = 65514;		// granica brojaèa – ili broj od kojeg se kreæe, pa se dekrementira,ili do kog treba da se doðe inkrementiranjem.
+MOVW	R1, #65514
+MOVW	R0, #lo_addr(TIM2_ARR+0)
+MOVT	R0, #hi_addr(TIM2_ARR+0)
+STR	R1, [R0, #0]
+;screen.c,56 :: 		NVIC_IntEnable(IVT_INT_TIM2); // omoguæavamo prekid IVT_INT_TIM2
+MOVW	R0, #44
+BL	_NVIC_IntEnable+0
+;screen.c,57 :: 		TIM2_DIER.UIE = 1;            // omoguæavamo update prekidza TIM2
+MOVS	R1, #1
+SXTB	R1, R1
+MOVW	R0, #lo_addr(TIM2_DIER+0)
+MOVT	R0, #hi_addr(TIM2_DIER+0)
+STR	R1, [R0, #0]
+;screen.c,58 :: 		TIM2_CR1.CEN = 1;             // ponovo ukljuèujemo TIM2
+MOVW	R0, #lo_addr(TIM2_CR1+0)
+MOVT	R0, #hi_addr(TIM2_CR1+0)
+STR	R1, [R0, #0]
+;screen.c,59 :: 		HID_Enable(&readbuff,&writebuff);
+MOVW	R1, #lo_addr(_writebuff+0)
+MOVT	R1, #hi_addr(_writebuff+0)
+MOVW	R0, #lo_addr(_readbuff+0)
+MOVT	R0, #hi_addr(_readbuff+0)
+BL	_HID_Enable+0
+;screen.c,60 :: 		}
+L_end_Initialize:
+LDR	LR, [SP, #0]
+ADD	SP, SP, #4
+BX	LR
+; end of _Initialize
+_main:
+;screen.c,62 :: 		void main(void) {
+;screen.c,64 :: 		Initialize();
+BL	_Initialize+0
+;screen.c,65 :: 		while (1) {
+L_main1:
+;screen.c,67 :: 		if (!HID_Read() && curr_state == STATE_RUNNING) {
+BL	_HID_Read+0
+CMP	R0, #0
+IT	NE
+BNE	L__main15
+MOVW	R0, #lo_addr(_curr_state+0)
+MOVT	R0, #hi_addr(_curr_state+0)
+LDRSH	R0, [R0, #0]
+CMP	R0, #1
+IT	NE
+BNE	L__main14
+L__main13:
+;screen.c,68 :: 		writebuff[0] = seconds;
+MOVW	R0, #lo_addr(_seconds+0)
+MOVT	R0, #hi_addr(_seconds+0)
+LDRSH	R1, [R0, #0]
+MOVW	R0, #lo_addr(_writebuff+0)
+MOVT	R0, #hi_addr(_writebuff+0)
+STRH	R1, [R0, #0]
+;screen.c,69 :: 		while(!HID_Write(&writebuff,64))
+L_main6:
+MOVS	R1, #64
+MOVW	R0, #lo_addr(_writebuff+0)
+MOVT	R0, #hi_addr(_writebuff+0)
+BL	_HID_Write+0
+CMP	R0, #0
+IT	NE
+BNE	L_main7
+;screen.c,70 :: 		;
+IT	AL
+BAL	L_main6
+L_main7:
+;screen.c,71 :: 		continue;
+IT	AL
+BAL	L_main1
+;screen.c,67 :: 		if (!HID_Read() && curr_state == STATE_RUNNING) {
+L__main15:
+L__main14:
+;screen.c,73 :: 		cnt = readbuff[0];
+MOVW	R0, #lo_addr(_readbuff+0)
+MOVT	R0, #hi_addr(_readbuff+0)
+; cnt start address is: 4 (R1)
+LDRB	R1, [R0, #0]
+;screen.c,75 :: 		if(cnt == MSG_START) {
+LDRB	R0, [R0, #0]
+CMP	R0, #83
+IT	NE
+BNE	L_main8
+; cnt end address is: 4 (R1)
+;screen.c,76 :: 		curr_state = STATE_RUNNING;
+MOVS	R1, #1
+SXTH	R1, R1
+MOVW	R0, #lo_addr(_curr_state+0)
+MOVT	R0, #hi_addr(_curr_state+0)
+STRH	R1, [R0, #0]
+;screen.c,77 :: 		} else if (cnt == MSG_RESET) {
+IT	AL
+BAL	L_main9
+L_main8:
+; cnt start address is: 4 (R1)
+CMP	R1, #82
+IT	NE
+BNE	L_main10
+; cnt end address is: 4 (R1)
+;screen.c,78 :: 		seconds = 0;
+MOVS	R1, #0
+SXTH	R1, R1
+MOVW	R0, #lo_addr(_seconds+0)
+MOVT	R0, #hi_addr(_seconds+0)
+STRH	R1, [R0, #0]
+;screen.c,79 :: 		} else if (cnt == MSG_PAUSE) {
+IT	AL
+BAL	L_main11
+L_main10:
+; cnt start address is: 4 (R1)
+CMP	R1, #80
+IT	NE
+BNE	L_main12
+; cnt end address is: 4 (R1)
+;screen.c,80 :: 		curr_state = STATE_PAUSE;
+MOVS	R1, #2
+SXTH	R1, R1
+MOVW	R0, #lo_addr(_curr_state+0)
+MOVT	R0, #hi_addr(_curr_state+0)
+STRH	R1, [R0, #0]
+;screen.c,81 :: 		}
+L_main12:
+L_main11:
+L_main9:
+;screen.c,82 :: 		}
+IT	AL
+BAL	L_main1
+;screen.c,83 :: 		}
+L_end_main:
+L__main_end_loop:
+B	L__main_end_loop
+; end of _main
--- master/screen.c
+++ master/screen.c
@@ -24,216 +24,60 @@
       - Turn ON Back Light switches at SW11.
 */
 
-#define COUNTERS 4
-#define COUNTER_PADDING 10
-#define INITIAL_POS_X 32
-#define INITIAL_POS_Y 0
-#define STOPWATCH_BUFFER_SIZE 10
 #define STATE_RUNNING 1
 #define STATE_PAUSE 2
 
-#define B_START 1
-#define B_LAP 2
-#define B_RESET 3
-#define B_PAUSE 4
-#define B_SAVE 5
+#define MSG_START 'S'
+#define MSG_RESET 'R'
+#define MSG_PAUSE 'P'
 
-// Glcd module connections
-unsigned long GLCD_DataPort_Input  at GPIOE_IDR;
-unsigned long GLCD_DataPort_Output at GPIOE_ODR;
-
-sbit GLCD_CS1           at GPIOE_ODR.B10;
-sbit GLCD_CS2           at GPIOE_ODR.B11;
-sbit GLCD_RS            at GPIOE_ODR.B12;
-sbit GLCD_RW            at GPIOE_ODR.B13;
-sbit GLCD_EN            at GPIOE_ODR.B15;
-sbit GLCD_RST           at GPIOE_ODR.B8;
-// End Glcd module connections
-
-bit          write_erase;
-char         pen_size;
-char start_msg[] = "S";
-char reset_msg[] = "R";
-char pause_msg[] = "P";
-int readbuff[32];
-char writebuff[64];
+int seconds = 0;
+char readbuff[64];
+int writebuff[32];
 int curr_state = STATE_PAUSE;
-int prev_b = 0;
-
-int times[COUNTERS];
-unsigned int x_coord, y_coord;
 
 void USB0Interrupt() iv IVT_INT_OTG_FS{
   USB_Interrupt_Proc();
 }
 
-void shift_timers(int keep_curr) {
-    int i, temp, t;
-    t = times[0];
-
-    for(i = 1; i < COUNTERS; i++) {
-        temp = times[i];
-        times[i] = t;
-        t = temp;
-    }
-
-    if (!keep_curr) {
-        times[0] = 0;
-    }
-}
-
-void print_timers() {
-	int i, h, m, s, temp;
-	int pos_y = INITIAL_POS_Y;
-	char str[STOPWATCH_BUFFER_SIZE];
-
-	Glcd_Fill(0); // Clear GLCD
-	delay10ms();
-
-	for(i = 0; i < COUNTERS; i++) {
-		temp = times[i];
-
-		h = temp / 3600;
-		temp %= 3600;
-
-		m = temp / 60;
-		temp %= 60;
-
-		s = temp;
-
-		sprintf(str, "%02d:%02d:%02d", h, m, s);
-		Glcd_Write_Text(str, INITIAL_POS_X, pos_y, 1);
-		delay10ms();
-		pos_y += 2;
-	}
+void Timer2_interrupt() iv IVT_INT_TIM2 {	// iv-> hendler za tajmerski prekid
+  TIM2_SR.UIF = 0;				//Kada se prekid desi, postavlja se fleg TIM2_SR.UIF
+  if(curr_state == STATE_RUNNING) {
+      seconds++;
+  }
 }
 
 void Initialize() {
-    int i;
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_0); // Set PA0 as start
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_1); // Set PA1 as lap
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_2); // Set PA2 as reset
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_3); // Set PA3 as pause
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_4); // Set PA4 as save
-
+    RCC_APB1ENR.TIM2EN = 1;       // koristimo TIM2
+    TIM2_CR1.CEN = 0;             // privremeno iskljuèujemo TIM2
+    TIM2_PSC = 1098;              // podesavamo preskaler(tj dvobajtnu vrednost sa kojom se deli frekvencija tajmera(tj TIM2_ARR) )
+    TIM2_ARR = 65514;		// granica brojaèa – ili broj od kojeg se kreæe, pa se dekrementira,ili do kog treba da se doðe inkrementiranjem.
+					//   65514/ 1098 = 59,66667 (tj 1 sekunda)
+    NVIC_IntEnable(IVT_INT_TIM2); // omoguæavamo prekid IVT_INT_TIM2
+    TIM2_DIER.UIE = 1;            // omoguæavamo update prekidza TIM2
+    TIM2_CR1.CEN = 1;             // ponovo ukljuèujemo TIM2
     HID_Enable(&readbuff,&writebuff);
-
-    Glcd_Init();                                     // Initialize GLCD
-    Glcd_Fill(0);                                    // Clear GLCD
-
-    for(i = 0; i < COUNTERS; i++) {
-        times[i] = 0;
-    }
-
-    print_timers();
-}
-
-void delay10ms() {
-  Delay_ms(10);
-}
-
-int check_start() {
-    if (prev_b == B_START) {
-        return 0;
-    }
-
-    return Button(&GPIOA_IDR, 0, 1, 1);
-}
-
-int check_lap() {
-    if (prev_b == B_LAP) {
-        return 0;
-    }
-
-    return Button(&GPIOA_IDR, 1, 1, 1);
-}
-
-int check_reset() {
-    if (prev_b == B_RESET) {
-        return 0;
-    }
-
-    return Button(&GPIOA_IDR, 2, 1, 1);
-}
-
-int check_pause() {
-    if (prev_b == B_PAUSE) {
-        return 0;
-    }
-
-    return Button(&GPIOA_IDR, 3, 1, 1);
-}
-
-int check_save() {
-    if (prev_b == B_SAVE) {
-        return 0;
-    }
-
-    return Button(&GPIOA_IDR, 4, 1, 1);
-}
-
-void start_timer() {
-    writebuff[0] = start_msg[0];
-    while(!HID_Write(&writebuff,64)); // Send the message
-}
-
-void pause_timer() {
-    writebuff[0] = pause_msg[0];
-    while(!HID_Write(&writebuff,64));
-}
-
-void save_timer() {
-    shift_timers(1);
-}
-
-void reset_timer() {
-    writebuff[0] = reset_msg[0];
-    while(!HID_Write(&writebuff,64));
-}
-
-void update_time() {
-    times[0] = readbuff[0];
-}
-
-void lap_timer() {
-    reset_timer();
-    shift_timers(0);
-    start_timer();
 }
 
 void main(void) {
   char cnt;
   Initialize();
-
     while (1) {
         // call print_timers() inside this loop.
-        if (HID_Read()) { // this won't hang because we are using async interrupts
-            if (curr_state == STATE_RUNNING) { // If we are not in a running state we just drop the messsage. @TODO: Check if we can skip HID_Read() all together.
-                update_time(); // check state or drop message
-                print_timers();
-            }
-        } else {
-            if (curr_state == STATE_RUNNING) {
-                if (check_pause()) {
-                    pause_timer();
-                } else if (check_lap()) {
-                    lap_timer();
-                    print_timers();
-                } else if (check_save()) {
-                    save_timer();
-                    print_timers();
-                } // Here we can stop, lap, save or pause.
-            } else if (curr_state == STATE_PAUSE) {
-                if (check_start()) {
-                    start_timer();
-                } else if (check_lap()) {
-                    lap_timer();
-                    print_timers();
-                } else if (check_save()) {
-                    save_timer();
-                    print_timers();
-                } // Here we can start, save
-            }
+        if (!HID_Read() && curr_state == STATE_RUNNING) {
+           writebuff[0] = seconds;
+           while(!HID_Write(&writebuff,64))
+           ;
+           continue;
+        }
+        cnt = readbuff[0];
+        
+        if(cnt == MSG_START) {
+            curr_state = STATE_RUNNING;
+        } else if (cnt == MSG_RESET) {
+            seconds = 0;
+        } else if (cnt == MSG_PAUSE) {
+            curr_state = STATE_PAUSE;
         }
     }
 }
