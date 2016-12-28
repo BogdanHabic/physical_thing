ebee98519b1f3d6c8a5b62d01c7cfda94f38351f
BogdanHabic <habicbogdan@gmail.com>
2016-12-28 16:15:25 +0100

    Initial commit for the target practice project

--- slave/screen.c
+++ slave/screen.c
@@ -24,19 +24,7 @@
       - Turn ON Back Light switches at SW11.
 */
 
-#define COUNTERS 4
-#define COUNTER_PADDING 10
-#define INITIAL_POS_X 32
-#define INITIAL_POS_Y 30
-#define STOPWATCH_BUFFER_SIZE 10
-#define STATE_RUNNING 1
-#define STATE_PAUSE 2
-
-#define B_START 1
-#define B_LAP 2
-#define B_RESET 3
-#define B_PAUSE 4
-#define B_SAVE 5
+#define CIRCLE_RADIUS 40
 
 // TFT module connections
 unsigned int TFT_DataPort at GPIOE_ODR;
@@ -50,234 +38,89 @@ sbit TFT_BLED at GPIOE_ODR.B9;
 
 bit          write_erase;
 char         pen_size;
-char start_msg[] = "S";
-char reset_msg[] = "R";
-char pause_msg[] = "P";
 int readbuff[32];
-char writebuff[64];
-int curr_state = STATE_PAUSE;
-int prev_b = 0;
-int timers_shifted = 0;
-
-int times[COUNTERS];
-unsigned int x_coord, y_coord;
+int writebuff[32];
 
 void USB0Interrupt() iv IVT_INT_OTG_FS{
   USB_Interrupt_Proc();
 }
 
-void delay10ms() {
-  Delay_ms(10);
+void set_brush_for_draw() {
+    TFT_Set_Brush(1, CL_FUCHSIA, 0, LEFT_TO_RIGHT, CL_AQUA, CL_AQUA);
 }
 
-void shift_timers(int keep_curr) {
-    int i, temp, t;
-    t = times[0];
-
-    for(i = 1; i < COUNTERS; i++) {
-        temp = times[i];
-        times[i] = t;
-        t = temp;
-    }
-
-    if (!keep_curr) {
-        times[0] = 0;
-    }
-    
-    timers_shifted = 1;
+void set_brush_for_fake_draw() {
+    TFT_Set_Brush(1, CL_BLACK, 0, LEFT_TO_RIGHT, CL_AQUA, CL_AQUA);
 }
 
-void print_timers() {
-        int i, h, m, s, temp;
-        int pos_y = INITIAL_POS_Y;
-        char str[STOPWATCH_BUFFER_SIZE];
+void set_brush_for_delete() {
+    TFT_Set_Brush(1, CL_AQUA, 0, LEFT_TO_RIGHT, CL_AQUA, CL_AQUA);
+}
 
-        if(timers_shifted) {
-            TFT_Fill_Screen(CL_AQUA); // Clear TFT
-            timers_shifted = 0;
-        }
+void draw_target() {
+    int x, y, del, fake;
 
-        TFT_Rectangle(20, 30, 219, 50);
+    x    = readbuff[0];
+    y    = readbuff[1];
+    del  = readbuff[2];
+    fake = readbuff[3];
 
-        for(i = 0; i < COUNTERS; i++) {
-                temp = times[i];
+    if(x == -1 || y == - 1 || del == 1) {
+        return;
+    }
 
-                h = temp / 3600;
-                temp %= 3600;
+    if (!fake) {
+        set_brush_for_draw();
+    } else {
+        set_brush_for_fake_draw();
+    }
 
-                m = temp / 60;
-                temp %= 60;
+    TFT_Circle(x, y, CIRCLE_RADIUS);
+}
 
-                s = temp;
+void delete_target() {
+    int x, y, del;
 
-                sprintf(str, "%02d:%02d:%02d", h, m, s);
-                TFT_Write_Text(str, INITIAL_POS_X, pos_y);
-                delay10ms();
-                pos_y += 20;
-        }
+    x   = readbuff[0];
+    y   = readbuff[1];
+    del = readbuff[2];
+
+    if(x == -1 || y == - 1 || del == 0) {
+        return;
+    }
+
+    set_brush_for_delete();
+    TFT_Circle(x, y, CIRCLE_RADIUS);
+}
+
+void send_touch(int x, int y) {
+    writebuff[0] = x;
+    writebuff[1] = y;
+
+    while(!HID_Write(&writebuff,64)); // Send the message
 }
 
 void Initialize() {
     int i;
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_0); // Set PA0 as start
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_6); // Set PA6 as lap
-    GPIO_Digital_Input(&GPIOD_IDR, _GPIO_PINMASK_4); // Set PD4 as reset
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_5); // Set PA5 as pause
-    GPIO_Digital_Input(&GPIOA_IDR, _GPIO_PINMASK_4); // Set PA4 as save
-    
+
     TFT_Init_ILI9341_8bit(320, 240);
     TFT_Set_Pen(CL_AQUA, 3);
     TFT_Set_Font(TFT_defaultFont, CL_WHITE, FO_HORIZONTAL);
     TFT_Fill_Screen(CL_AQUA);
-    TFT_Set_Brush(1, CL_BLACK, 0, LEFT_TO_RIGHT, CL_AQUA, CL_AQUA);
+    TFT_Set_Brush(1, CL_AQUA, 0, LEFT_TO_RIGHT, CL_AQUA, CL_AQUA);
 
     HID_Enable(&readbuff,&writebuff);
-
-    for(i = 0; i < COUNTERS; i++) {
-        times[i] = 0;
-    }
-
-    print_timers();
-}
-
-int check_start() {
-    int val;
-    if (prev_b == B_START) {
-        return 0;
-    }
-
-    val = Button(&GPIOA_IDR, 0, 1, 1);
-    if(val) prev_b = B_START;
-
-    return val;
-}
-
-int check_lap() {
-    int val;
-    if (prev_b == B_LAP) {
-        return 0;
-    }
-    
-    val = Button(&GPIOA_IDR, 6, 1, 1);
-    if(val) prev_b = B_LAP;
-
-    return val;
-}
-
-int check_reset() {
-    int val;
-    if (prev_b == B_RESET) {
-        return 0;
-    }
-    
-    val = Button(&GPIOD_IDR, 4, 1, 1);
-    if(val) prev_b = B_RESET;
-
-    return val;
-}
-
-int check_pause() {
-    int val;
-    if (prev_b == B_PAUSE) {
-        return 0;
-    }
-
-    val = Button(&GPIOA_IDR, 5, 1, 1);
-    if(val) prev_b = B_PAUSE;
-
-    return val;
-}
-
-int check_save() {
-    int val;
-    if (prev_b == B_SAVE) {
-        return 0;
-    }
-
-    val = Button(&GPIOA_IDR, 4, 1, 1);
-    if(val) prev_b = B_SAVE;
-
-    return val;
-}
-
-void start_timer() {
-    writebuff[0] = start_msg[0];
-    TFT_Write_Text("START", 100, 100);
-    while(!HID_Write(&writebuff,64)); // Send the message
-    curr_state = STATE_RUNNING;
-}
-
-void pause_timer() {
-//    TFT_Write_Text("USAO", 150, 100);
-    writebuff[0] = pause_msg[0];
-
-    TFT_Write_Text("PAUSE", 120, 120);
-    while(!HID_Write(&writebuff,64));
-    curr_state = STATE_PAUSE;
-}
-
-void save_timer() {
-    shift_timers(1);
-}
-
-void reset_timer() {
-    writebuff[0] = reset_msg[0];
-    TFT_Write_Text("RESET", 100, 140);
-    while(!HID_Write(&writebuff,64));
-}
-
-void update_time() {
-    times[0] = readbuff[0];
-}
-
-void lap_timer() {
-
-    TFT_Write_Text("LAP", 100, 160);
-    reset_timer();
-    shift_timers(0);
-    start_timer();
 }
 
 void main(void) {
-  char cnt;
   Initialize();
 
     while (1) {
-        // call print_timers() inside this loop.
-
         if (HID_Read()) { // this won't hang because we are using async interrupts
-            if (curr_state == STATE_RUNNING) {
-             // If we are not in a running state we just drop the messsage. @TODO: Check if we can skip HID_Read() all together.
-                update_time(); // check state or drop message
-                print_timers();
-            }
+            draw_target();
+            delete_target();
         } else {
-            if (curr_state == STATE_RUNNING) {
-                if (check_pause()) {
-                    pause_timer();
-                } else if (check_lap()) {
-                    lap_timer();
-                    print_timers();
-                } else if (check_save()) {
-                    save_timer();
-                    print_timers();
-                } // Here we can stop, lap, save or pause.
-            } else if (curr_state == STATE_PAUSE) {
-                if (check_start()) {
-                    start_timer();
-                } else if (check_lap()) {
-                    lap_timer();
-                    print_timers();
-                } else if (check_save()) {
-                    save_timer();
-                    print_timers();
-                } else if (check_reset()) {
-                    TFT_Write_Text("USAO", 150, 100);
-                    reset_timer();
-                    times[0] = 0;
-                    print_timers();
-                }// Here we can start, save
-            }
+            //@TODO Check for a touch
         }
     }
 }
