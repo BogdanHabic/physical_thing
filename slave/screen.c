10294abe7bd228a81c186cafaa7247766ed5891e
BogdanHabic <habicbogdan@gmail.com>
2016-12-28 17:01:52 +0100

    Added initial code for touch panel

--- slave/screen.c
+++ slave/screen.c
@@ -36,10 +36,21 @@ sbit TFT_WR at GPIOE_ODR.B11;
 sbit TFT_BLED at GPIOE_ODR.B9;
 // End TFT module connections
 
+// Touch Panel module connections
+sbit DriveX_Left at GPIOB_ODR.B1;
+sbit DriveX_Right at GPIOB_ODR.B8;
+sbit DriveY_Up at GPIOB_ODR.B9;
+sbit DriveY_Down at GPIOB_ODR.B0;
+// End Touch Panel module connections
+
 bit          write_erase;
 char         pen_size;
 int readbuff[32];
 int writebuff[32];
+
+unsigned int Xcoord, Ycoord;
+const ADC_THRESHOLD = 750;
+char PenDown;
 
 void USB0Interrupt() iv IVT_INT_OTG_FS{
   USB_Interrupt_Proc();
@@ -100,10 +111,79 @@ void send_touch(int x, int y) {
     while(!HID_Write(&writebuff,64)); // Send the message
 }
 
-void Initialize() {
-    int i;
+void Calibrate() {
+  TFT_Set_Pen(CL_WHITE, 3);
+  TFT_Set_Font(TFT_defaultFont, CL_WHITE, FO_HORIZONTAL);
+  TFT_Write_Text("Touch selected corners for calibration", 50, 80);
+  TFT_Line(315, 239, 319, 239);
+  TFT_Line(309, 229, 319, 239);
+  TFT_Line(319, 234, 319, 239);
+  TFT_Write_Text("first here",235,220);
 
-    TFT_Init_ILI9341_8bit(320, 240);
+  TP_TFT_Calibrate_Min();                      // Calibration of bottom left corner
+  Delay_ms(500);
+
+  TFT_Set_Pen(CL_BLACK, 3);
+  TFT_Set_Font(TFT_defaultFont, CL_BLACK, FO_HORIZONTAL);
+  TFT_Line(315, 239, 319, 239);
+  TFT_Line(309, 229, 319, 239);
+  TFT_Line(319, 234, 319, 239);
+  TFT_Write_Text("first here",235,220);
+
+  TFT_Set_Pen(CL_WHITE, 3);
+  TFT_Set_Font(TFT_defaultFont, CL_WHITE, FO_HORIZONTAL);
+  TFT_Write_Text("now here ", 20, 5);
+  TFT_Line(0, 0, 5, 0);
+  TFT_Line(0, 0, 0, 5);
+  TFT_Line(0, 0, 10, 10);
+
+  TP_TFT_Calibrate_Max();                      // Calibration of bottom left corner
+  Delay_ms(500);
+}
+
+
+void Init_ADC() {
+  ADC_Set_Input_Channel(_ADC_CHANNEL_8 | _ADC_CHANNEL_9);
+  ADC1_Init();
+  Delay_ms(100);
+}
+
+static void InitializeTouchPanel() {
+  Init_ADC();
+  TFT_Init_ILI9341_8bit(320, 240);
+
+  TP_TFT_Init(320, 240, 8, 9);                 // Initialize touch panel
+  TP_TFT_Set_ADC_Threshold(ADC_THRESHOLD);     // Set touch panel ADC threshold
+
+  PenDown = 0;
+}
+
+void Init_MCU() {
+  GPIO_Config(&GPIOE_BASE, _GPIO_PINMASK_9, _GPIO_CFG_DIGITAL_OUTPUT);
+  TFT_BLED = 1;
+  TFT_Set_Default_Mode();
+  TP_TFT_Set_Default_Mode();
+}
+
+void Start_TP() {
+  Init_MCU();
+
+  InitializeTouchPanel();
+
+  Delay_ms(1000);
+  TFT_Fill_Screen(0);
+  Calibrate();
+  TFT_Fill_Screen(0);
+
+  InitializeObjects();
+  display_width = Screen2.Width;
+  display_height = Screen2.Height;
+  DrawScreen(&Screen2);
+}
+
+void Initialize() {
+    Start_TP(); // Initialize touch panel
+
     TFT_Set_Pen(CL_AQUA, 3);
     TFT_Set_Font(TFT_defaultFont, CL_WHITE, FO_HORIZONTAL);
     TFT_Fill_Screen(CL_AQUA);
